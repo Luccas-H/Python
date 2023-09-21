6e95237ba1a5769f4eba7d35df4cf7d249ac7376
Luccas Henrique <luccas.henrique.vr@gmail.com>
2023-09-21 16:56:55 -0400

    feature: add numerical order in list_num / del gem

--- primo_gem.py
+++ primo_gem.py
@@ -1,7 +1,6 @@
 n = int(input("Digite um número:"))
 m = int(input("Digite um número:"))
 list_num = []
-list_gem = []
 d = 2
 s = 0
 
@@ -9,19 +8,18 @@ list_num.append(n)
 list_num.append(m)
 
 for i in list_num:
-
+    list_num.sort(key=int)
+    
     while i%d != 0 and d*d <= i:
         d = d + 1 
     if d*d >= i:
         print("primo")
-        
     else:
         print("não primo")
-    list_num.sort()
-    s = i - s
-    if s == 2:
-        print("primo gem")
     d = 2    
 
-    
+for j in list_num:
+    s = j - s
+    if s == 2:
+        print("é gem")
     
