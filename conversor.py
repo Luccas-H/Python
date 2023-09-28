a47362d612cf5c96e1f22df43f45d10e46b32283
Luccas Henrique <luccas.henrique.vr@gmail.com>
2023-09-28 13:08:31 -0400

    conversor_medidas_pes.py

---
+++ conversor.py
@@ -0,0 +1,3 @@
+medida_pes = float(input("Digite a medida desejada:"))
+
+print('{} pês equivalem a {} polegadas, {} jardas, e {:.2} milhas'.format(medida_pes, medida_pes*12,medida_pes/3, medida_pes/5280))
