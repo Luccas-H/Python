906c648be9dddddc302b613eb7a5806de3b58791
Luccas Henrique <luccas.henrique.vr@gmail.com>
2023-09-28 12:14:48 -0400

    media_ponderada.py

---
+++ media_ponderada.py
@@ -0,0 +1,25 @@
+lista_notas = []
+lista_pesos = []
+lista_resu = []
+i = 0 
+
+while i!=3:
+    n = float(input("Digite uma nota: "))
+    p = int(input("Digite o peso dessa nota: "))
+    lista_notas.append(n)
+    lista_pesos.append(p)
+    i =  i + 1 
+print(lista_notas)
+print(lista_pesos)
+
+for i in range(len(lista_notas)):
+    tupla = (lista_notas[i], lista_pesos[i])
+    lista_resu.append(tupla)
+
+m = 0
+for k in range(len(lista_resu)):
+    m = lista_resu[k][0]*lista_resu[k][1] + m        
+
+print(m/sum(lista_pesos)) 
+
+ 
