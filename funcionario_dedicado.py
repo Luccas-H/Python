3e9bd1ab08a8f3f03fbd1ba7a597893836788bb6
Luccas Henrique <luccas.henrique.vr@gmail.com>
2023-10-09 19:18:00 -0400

    feature: add

--- funcionario_dedicado.py
+++ funcionario_dedicado.py
@@ -1,35 +1,46 @@
-#Não pronto
-import random
-funcionarios = [1,2,3,4,5]
-pontuacao = []
+import random 
 
-n = 0
-while n!=15:
-    p = random.randint(1,10)
-    pontuacao.append([p])
-    n += 1
-for i in pontuacao:
-    soma = 1
-    if pontuacao.index(i) >= 0 and pontuacao.index(i)<=3:
-        soma = i + soma
-    if pontuacao.index(i) > 3 and pontuacao.index(i)<=6:
-        soma = i + soma
-    if pontuacao.index(i) > 6 and pontuacao.index(i)<=9:
-        soma = i + soma
-    if pontuacao.index(i) > 9 and pontuacao.index(i)<=12:
-        soma = i + soma
-    if pontuacao.index(i) > 12 and pontuacao.index(i)<=15:
-        soma = i + soma
-    
-    
-   
-print(soma)
+pontos_total_cada = []
+media_cada = []
+list_funcionarios = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+list_pontos = []
+list_total = []
+maximo = 0 
+minimo = 0
+
+n = 0 
+while n !=45:
+    p = random.randint(1,15)
+    list_pontos.append(p)
+    n +=1 
+k = 15
+
+splited = []
+len_l = len(list_pontos)
+for i in range(k):
+    start = int(i*len_l/k)
+    end = int((i+1)*len_l/k)
+    splited.append(list_pontos[start:end])
 
 
+for s in range(len(splited)):
+    r = splited[s][0] + splited[s][1] + splited[s][2]  
+    pontos_total_cada.append(r)  
 
+print('Segue a baixo os valores de cada funcionário do 1 ao 15:\n')
 
+for j in range(len(pontos_total_cada)):
+        print('funcionário  -> {}'.format(pontos_total_cada[j]))
 
+for m in range(len(splited)):
+    r = (splited[m][0] + splited[m][1] + splited[m][2])/3  
+    media_cada.append(r)  
 
+print('Segue a baixo as médias de cada funcionário do 1 ao 15:\n')
 
+for j in range(len(media_cada)):
+        print('funcionário  -> {:.3}'.format(media_cada[j]))
 
-print(pontuacao)
+maximo = max(pontos_total_cada)
+posicao_maximo = pontos_total_cada.index(max(pontos_total_cada))
+print("\nA maior pontuação atingida foi pelo funcionário {} com um total de {} pontos".format(posicao_maximo + 1,maximo))
