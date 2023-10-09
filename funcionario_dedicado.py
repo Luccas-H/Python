63e9604705b3ceb3b149fc80d1f167c1d15fa8da
Luccas Henrique <luccas.henrique.vr@gmail.com>
2023-10-09 13:05:48 -0400

    feature: add

---
+++ funcionario_dedicado.py
@@ -0,0 +1,34 @@
+import random
+funcionarios = [1,2,3,4,5]
+pontuacao = []
+
+n = 0
+while n!=15:
+    p = random.randint(1,10)
+    pontuacao.append([p])
+    n += 1
+for i in pontuacao:
+    soma = 1
+    if pontuacao.index(i) >= 0 and pontuacao.index(i)<=3:
+        soma = i + soma
+    if pontuacao.index(i) > 3 and pontuacao.index(i)<=6:
+        soma = i + soma
+    if pontuacao.index(i) > 6 and pontuacao.index(i)<=9:
+        soma = i + soma
+    if pontuacao.index(i) > 9 and pontuacao.index(i)<=12:
+        soma = i + soma
+    if pontuacao.index(i) > 12 and pontuacao.index(i)<=15:
+        soma = i + soma
+    
+    
+   
+print(soma)
+
+
+
+
+
+
+
+
+print(pontuacao)
