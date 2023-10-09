90989584e7e591867e9ae5254f2b55f41c47c933
Luccas Henrique <luccas.henrique.vr@gmail.com>
2023-10-09 17:06:57 -0400

    feature: bug fixed

--- quadrante_angulo.py
+++ quadrante_angulo.py
@@ -1,21 +1,6 @@
 grau = int(input("Digite o angulo desejado: "))
 
 
-if grau < 0:
-    print("Sentido horário")
-    grau = grau%360
-    if grau <= 0 and grau >= -90:
-        print("Pertence ao quarto quadrante")
-
-    if grau < -90 and grau >= -180:
-        print("Pertence ao terceiro quadrante")
-
-    if grau < -180 and grau >= -270:
-        print("Pertence ao segundo quadrante")
-
-    if grau < -270 and grau >= -360:   
-        print("Pertence ao primeiro quadrante")
-        
 if grau > 360:
     print("anti-horário")
     print('O número de voltas é igual a {:.2}'.format(grau/360))
@@ -45,3 +30,24 @@ else:
     if grau > 270 and grau <= 360:   
         print("Pertence ao quarto quadrante")
 
+if grau < 0:
+    print("Sentido horário")
+    
+    if grau < -360:
+        print('O número de voltas é igual a {:.2}'.format(grau/360 * -1))
+    
+    grau = (grau*-1)%360
+    grau = grau * -1
+    
+    if grau <= 0 and grau >= -90:
+        print("Pertence ao quarto quadrante")
+
+    if grau < -90 and grau >= -180:
+        print("Pertence ao terceiro quadrante")
+
+    if grau < -180 and grau >= -270:
+        print("Pertence ao segundo quadrante")
+
+    if grau < -270 and grau >= -360:   
+        print("Pertence ao primeiro quadrante")
+        
