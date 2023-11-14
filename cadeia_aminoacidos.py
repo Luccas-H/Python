d84f9c2ace89ce73995bd087d1987dd257b29a01
Luccas Henrique <luccas.henrique.vr@gmail.com>
2023-11-14 18:21:14 -0400

    feature: finishing cadeia_aminoacido.py

--- cadeia_aminoacidos.py
+++ cadeia_aminoacidos.py
@@ -1,12 +1,9 @@
-cadeia_aminoacido = str(input("Digite uma cadeia de cadeia aminoácido: "))
-cadeia_separada = []
-
-cadeia_separada.extend(cadeia_aminoacido)
+cadeia_aminoacido = str(input("Com base nas trincas abaixo:\nUUU|CUU|UUA|AAG|UCU|UAU|CAA\nDigite uma cadeia de aminoácidos: "))
 
 rna_trinca = {
     'UUU' : 'Phe',
     'CUU' : 'Leu',
-    'UUA' : 'Leu',
+    'UUA' : 'Leucina',
     'AAG' : 'Lisina',
     'UCU' : 'Ser',
     'UAU' : 'Tyr',
@@ -21,42 +18,30 @@ incremento_pos_inicio = 3
 incremento_loop = 3
 
 while incremento_loop != 0:
-    list_trio = cadeia_separada[limite_inicio:incremento_pos_inicio:]
-    separador = ' '
-    resultado = [separador.join(list_trio)]
-    list_trincas.append(resultado)
+    list_trio = cadeia_aminoacido[limite_inicio:incremento_pos_inicio:]
+    list_trincas.append(list_trio)
     limite_inicio +=3
     incremento_pos_inicio+=3
     incremento_loop -=1
 
+valores = []
+for rna in rna_trinca:    
+    valores.append(rna_trinca.get(f"{rna}"))
+    def get_key (val):
+        for key, value in rna_trinca.items():
+            if val == value:
+                return key    
 
-def get_key (val):
-    for key, value in rna_trinca.items():
-        if val == value:
-            return key
-        
+chaves = []
+for valor in valores:
+    chaves.append(get_key(valor))
+    
 
-traduzido = ''
-len_l = len(list_trincas)
-incremento_ = len(list_trincas)
-for i in range(len_l):
-    for j in rna_trinca:
-        valores = rna_trinca.get(f"{j}")
-        print(valores)
-        print(get_key(valores))
-        while incremento_ !=0:
-            print(list_trincas[i])
-            if list_trincas[i] == get_key(valores):
-                traduzido = rna_trinca.get(f"{valores}")
-            incremento_ -=1        
+traducao = []
+for trinca in list_trincas:
+    if trinca in chaves:
+        traducao.append(rna_trinca.get(trinca))
 
-print(traduzido)
-print(rna_trinca)
-print(list_trincas)
+for trinca_traduzida in traducao:
+    print(f"{trinca_traduzida}", end ="")
 
-
-
-
-
-
-# Falta traduzir as trincas / list_trincas[i] ta dando erro precisa verificar
